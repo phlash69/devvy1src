90dce3e6d4293fecc956b47f903cce569b053091
Unknown <phlash@georgepechtol.com>
2018-02-12 09:47:09 -0500

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=devvy1-Web-Deploy.pubxml -p:Password=gLT1zjWk33i30c9aKC5Zp6YfaDsGigJwGcm0D6mn1m1RuBvmb7byzJax1nWd
+
